1dde2744bea721d22acabb35a1da609836882542
Michiel Lowijs <michiel@flex-id.nl>
2020-09-30 16:56:38 +0200

    add options

--- TeslaCam.Notifiers.Pushover/PushoverNotifier.cs
+++ TeslaCam.Notifiers.Pushover/PushoverNotifier.cs
@@ -1,10 +1,16 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using TeslaCam.Contracts;
 
 namespace TeslaCam.Notifiers.Pushover
 {
     public class PushoverNotifier : INotifier
     {
+        public PushoverNotifier(IOptions<PushoverOptions> pushoverOptions)
+        {
+                
+        }
+        
         public string Name => "pushover";
         
         public Task NotifyAsync(string message)
--- TeslaCam.Notifiers.Pushover/PushoverOptions.cs
+++ TeslaCam.Notifiers.Pushover/PushoverOptions.cs
@@ -2,6 +2,7 @@
 {
     public class PushoverOptions
     {
-        
+        public string ApiToken { get; set; }
+        public string UserKey { get; set; }
     }
 }
